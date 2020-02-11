acb48b4ffc11440db863836a5f76c10338d1d0cf
rxfxngel <34992925+rxfxngel@users.noreply.github.com>
2020-02-11 16:58:14 -0500

    Update CONSULTAS.sql

--- CONSULTAS.sql
+++ CONSULTAS.sql
@@ -1,4 +1,44 @@
---CLAUSULA WHERE
-SELECT employee_id, last_name, job_id, department_id
-FROM   employees
-WHERE  department_id = 90 ;
+--CLAUSULA WHERE (CUANDO FILTRA TEXTO TOMA EN CUENTA LAS MAYUSCULAS Y MINUSCULAS
+  SELECT employee_id, last_name, job_id, department_id
+  FROM   employees
+  WHERE  department_id = 90 ;
+--WHERE CON FECHA SE DEBE TOMAR EN CUENTA EL IDIOMA DE LA BASE DE DATOS ( FORMATO POR DEFECTO) DD-MON-RR.
+  SELECT last_name
+  FROM   employees
+  WHERE  hire_date = '17-OCT-03' ;
+--OPERADORES DE COMPARACION
+  SELECT last_name, salary
+  FROM   employees
+  WHERE  salary <= 3000 ;
+-- CONDICIONANDO RANGOS USANDO BETWEEN
+  SELECT last_name, salary
+  FROM   employees
+  WHERE  salary BETWEEN 2500 AND 3500 ;
+
+  SELECT last_name, salary
+  FROM   employees
+  WHERE  last_name BETWEEN 'B' AND 'D' ;
+-- USANDO EL OPERADOR IN
+  SELECT employee_id, last_name, salary, manager_id
+  FROM   employees
+  WHERE  manager_id IN (100, 101, 201) ;
+
+  SELECT employee_id, manager_id, department_id
+  FROM   employees
+  WHERE  last_name IN ('Hartstein', 'Vargas');
+-- OPERADOR LIKE (SI SE QUIERE USAR EL SIMBOLO _ O % anteponer el simbolo \ por ejemplo "like '\_A%';
+  SELECT	first_name
+  FROM 	employees
+  WHERE	first_name LIKE '_a%a_' OR first_name LIKE '_a_' ;
+  
+  SELECT last_name
+  FROM employees 
+  WHERE last_name
+  LIKE '%A\_B%' ESCAPE '\'
+  ORDER BY last_name;
+--CONDICION NULL
+  SELECT last_name, manager_id
+  FROM   employees
+  WHERE  manager_id IS NULL ;
+
+
