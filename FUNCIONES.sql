704514ec19c6fb2c9f047c79dc6bf80c4404e6ee
rxfxngel <34992925+rxfxngel@users.noreply.github.com>
2020-02-22 15:46:22 -0500

    Update FUNCIONES.sql

--- FUNCIONES.sql
+++ FUNCIONES.sql
@@ -8,3 +8,12 @@ SELECT last_name,  salary, commission_pct,
        NVL2(commission_pct, 
             'SAL+COMM', 'SAL') income
 FROM   employees WHERE department_id IN (50, 80);
+
+--NULLIF       NULLIF(LENGTH(valor1), LENGTH(valor2)) 
+--si valor 1=valor2 entonces retorna null caso contrario retorna el valor 1
+        
+SELECT first_name, LENGTH(first_name) "expr1", 
+       last_name,  LENGTH(last_name)  "expr2",
+       NULLIF(LENGTH(first_name), LENGTH(last_name)) result
+FROM   employees;
+
