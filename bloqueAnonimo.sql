956a4254c6fbd5d59674c1edfc4807e7c6f010cb
rxfxngel <34992925+rxfxngel@users.noreply.github.com>
2020-04-12 08:32:11 -0500

    Update bloqueAnonimo.sql

--- bloqueAnonimo.sql
+++ bloqueAnonimo.sql
@@ -21,3 +21,20 @@ BEGIN
     num2:=3;
     DBMS_OUTPUT.PUT_LINE(num1+num2);
 END;
+
+SET SERVEROUTPUT ON
+
+DECLARE
+    name VARCHAR2(100);
+    age INT;
+    birthday DATE;
+    
+BEGIN
+    name:='RAFAEL';
+    age:=27;
+    birthday:=TO_DATE('07/04/1993','DD/MM/YYYY');
+    DBMS_OUTPUT.PUT_LINE(name);
+    DBMS_OUTPUT.PUT_LINE(age);
+    DBMS_OUTPUT.PUT_LINE(birthday);
+    DBMS_OUTPUT.PUT_LINE(name||age||birthday);
+END;
