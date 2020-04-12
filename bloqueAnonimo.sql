c28b23852d0a838c9205d61b4d31bcd250cd0db7
rxfxngel <34992925+rxfxngel@users.noreply.github.com>
2020-04-12 08:15:04 -0500

    Update bloqueAnonimo.sql

--- bloqueAnonimo.sql
+++ bloqueAnonimo.sql
@@ -1,1 +1,10 @@
+SET SERVEROUTPUT ON
 
+DECLARE-- CTRL + F7 PARA FORMATEAR
+    x   CONSTANT NUMBER := 10;
+    z   NUMBER NOT NULL := 20;
+BEGIN
+    dbms_output.put_line(x);
+    z := 30;
+    dbms_output.put_line(z);
+END;
