5e36f329dc24ae4a6bb8825ef8f5acf70602d6af
rxfxngel <34992925+rxfxngel@users.noreply.github.com>
2020-04-12 08:20:51 -0500

    ss
    ss

--- bloqueAnonimo.sql
+++ bloqueAnonimo.sql
@@ -9,3 +9,17 @@ BEGIN
     z := 30;
     dbms_output.put_line(z);
 END;
+
+
+SET SERVEROUTPUT ON
+
+DECLARE
+    nombre     VARCHAR2(100);
+    apellido   VARCHAR2(100);
+BEGIN
+    nombre := 'JUAN';
+    apellido := 'PEREZ';
+    DBMS_OUTPUT.PUT_LINE(nombre);
+    DBMS_OUTPUT.PUT_LINE(apellido);
+     DBMS_OUTPUT.PUT_LINE(nombre||' '||apellido);
+END;
