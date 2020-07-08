8b5b597af5e3b020c2df22c533ad0e885e63d89d
rxfxngel <34992925+rxfxngel@users.noreply.github.com>
2020-07-08 08:51:13 -0500

    Update capitulo4.sql

--- capitulo4.sql
+++ capitulo4.sql
@@ -69,10 +69,92 @@ END [package_name];
 
 
 /*
-La opción O REEMPLAZAR cae y vuelve a crear el
-especificación del paquete
+La opción O REEMPLAZAR elimina y vuelve a crear la especificación del paquete
 • Las variables declaradas en la especificación del paquete son
 inicializado a NULL por defecto.
 • Todas las construcciones declaradas en una especificación de paquete son
 visible para los usuarios que tienen privilegios en el paquete.
 */
+-- EJEMPLO DE LA ESPECIFICACION DEL PAQUETE
+
+-- The package spec with a public variable and a
+-- public procedure that are accessible from
+-- outside the package.
+
+CREATE OR REPLACE PACKAGE comm_pkg IS
+v_std_comm NUMBER := 0.10; --initialized to 0.10
+PROCEDURE reset_comm(p_new_comm NUMBER);
+END comm_pkg;
+/
+--------------------package body
+
+CREATE [OR REPLACE] PACKAGE BODY package_name IS|AS
+private type and variable declarations
+subprogram bodies
+[BEGIN initialization statements]
+END [package_name];
+
+
+/*
+La opción OR REPLACE cae y vuelve a crear el
+cuerpo del paquete
+• Los identificadores definidos en el cuerpo del paquete son privados y
+no visible fuera del cuerpo del paquete.
+• Todas las construcciones privadas deben declararse antes de ser
+referenciado
+• Las construcciones públicas son visibles para el cuerpo del paquete.
+
+*/
+
+--[[[[[[[Ejemplo package BODY]]]]]]]]]]
+
+CREATE OR REPLACE PACKAGE BODY comm_pkg IS
+FUNCTION validate(p_comm NUMBER) RETURN BOOLEAN IS
+v_max_comm employees.commission_pct%type;
+BEGIN
+SELECT MAX(commission_pct) INTO v_max_comm
+FROM employees;
+RETURN (p_comm BETWEEN 0.0 AND v_max_comm);
+END validate;
+PROCEDURE reset_comm (p_new_comm NUMBER) IS
+BEGIN
+IF validate(p_new_comm) THEN
+v_std_comm := p_new_comm; -- reset public var
+ELSE RAISE_APPLICATION_ERROR(
+-20210, 'Bad Commission');
+END IF;
+END reset_comm;
+END comm_pkg;
+
+
+
+--................ejemplo package con constantes
+
+CREATE OR REPLACE PACKAGE global_consts IS
+c_mile_2_kilo CONSTANT NUMBER := 1.6093;
+c_kilo_2_mile CONSTANT NUMBER := 0.6214;
+c_yard_2_meter CONSTANT NUMBER := 0.9144;
+c_meter_2_yard CONSTANT NUMBER := 1.0936;
+END global_consts;
+
+--ejecucion del ejemplo
+
+SET SERVEROUTPUT ON
+BEGIN
+DBMS_OUTPUT.PUT_LINE('20 miles = ' ||
+20 * global_consts.c_mile_2_kilo || ' km');
+END;
+
+
+--................funcion usando constante del paquete
+
+SET SERVEROUTPUT ON
+CREATE FUNCTION mtr2yrd(p_m NUMBER) RETURN NUMBER IS
+BEGIN
+RETURN (p_m * global_consts.c_meter_2_yard);
+END mtr2yrd;
+/
+
+--ejecutando la funcion
+EXECUTE DBMS_OUTPUT.PUT_LINE(mtr2yrd(1))
+
